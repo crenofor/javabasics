e72fbeb5dfe5c9d179853bc96233d90fafea8d0f
crenofor <crenofor@gmail.com>
2025-08-20 08:01:15 -0300

    correção

--- src/org/crenofor/maratonajava/introducao/A006_RepetitiveStructures02.java
+++ src/org/crenofor/maratonajava/introducao/A006_RepetitiveStructures02.java
@@ -2,7 +2,7 @@ package org.crenofor.maratonajava.introducao;
 
 public class A006_RepetitiveStructures02 {
     public static void main(String[] args) {
-        //imprima todso os numeros pares ate o numero 1.000.000
+        //imprima todos os numeros pares ate o numero 1.000.000
         for (int i = 999999; i <= 1000000; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
